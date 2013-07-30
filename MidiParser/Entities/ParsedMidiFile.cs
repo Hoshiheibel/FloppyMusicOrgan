b39408e94f9c0690ba75522161f9c05eff50c39e
HoshiHeibel <b.bandleon@vescon.com>
2013-07-30 17:59:16 +0200

    added file

---
+++ MidiParser/Entities/ParsedMidiFile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MidiParser.Entities
+{
+    public class ParsedMidiFile
+    {
+        public MidiHeader MidiHeader { get; private set; }
+        public List<MidiTrack> Tracks { get; internal set; }
+
+        internal ParsedMidiFile()
+        {
+            MidiHeader = new MidiHeader();
+            Tracks = new List<MidiTrack>();
+        }
+    }
+}
