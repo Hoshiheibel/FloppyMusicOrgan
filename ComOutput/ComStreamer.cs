424bd22f22344dc81ef6b2bcdd60501e8135beba
HoshiHeibel <b.bandleon@vescon.com>
2014-03-27 09:40:14 +0100

    Removed unused using / moved file

--- ComOutput/ComStreamer.cs
+++ ComOutput/ComStreamer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
 using System.Threading;
-using MidiParser.Entities;
 using MidiToArduinoConverter;
 
 namespace ComOutput
---
+++ MidiToArduinoConverter/ConvertedMidiTrack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using MidiParser.Entities.MidiFile;
+
+namespace MidiToArduinoConverter
+{
+    public class ConvertedMidiTrack
+    {
+        public MidiFile MidiFile { get; set; }
+        public List<ArduinoMessage> MessageList { get; set; }
+        public int BPM { get; set; }
+
+        public ConvertedMidiTrack()
+        {
+            MessageList = new List<ArduinoMessage>();
+        }
+    }
+}
