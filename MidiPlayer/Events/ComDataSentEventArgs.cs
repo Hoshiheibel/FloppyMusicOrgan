d43f5711740debbd4a5d6316dbb975ea761d1f91
HoshiHeibel <b.bandleon@vescon.com>
2014-06-23 08:51:10 +0200

    Added missing file

---
+++ MidiPlayer/Events/ComDataSentEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using MidiParser.Entities.MidiEvents;
+
+namespace MidiPlayer.Events
+{
+    public class ComDataSentEventArgs : EventArgs
+    {
+        public List<BaseMidiChannelEvent> Messages { get; set; }
+    }
+}
